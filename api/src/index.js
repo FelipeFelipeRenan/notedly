98ae6cbaef9231f28099502c426068055edd7563
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-01-18 18:11:19 -0300

    [DEV] Adding GraphQL first setup

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,31 @@
+const { ApolloServer, gql } = require('apollo-server-express')
+
 const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => { res.send('Hello World!!!') })
+// Construir um schema, usando a linguagem de schema do GraphQL
+const typeDefs = gql `
+    type Query {
+        hello: String
+    }
+`;
+// Funçoes resolver para os campos do schema criado anteriormente
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world'
+    }
+}
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+// Setup do Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers })
+
+// Aplicando a Middleware do Apollo GraphQL e pondo o caminho para /api
+server.applyMiddleware({ app, path: '/api' })
+
+
+//app.get('/', (req, res) => { res.send('Hello World!!!') })
+
+app.listen({ port }, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`))
