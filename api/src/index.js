cb3ef9c86e50389db4402aeb0ab6458071d51b75
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-01-18 19:30:06 -0300

    [DEV] Adding Query and Resolvers to API

--- api/src/index.js
+++ api/src/index.js
@@ -6,16 +6,37 @@ const app = express()
 
 const port = process.env.PORT || 4000
 
+let notes = [
+    { id: '1', content: 'This is a note', author: "Adam Scott" },
+    { id: '2', content: 'This is another note', author: "Harlow Everly" },
+    { id: '3', content: 'Oh hey look, another note', author: "Riley Harrison" }
+]
+
 // Construir um schema, usando a linguagem de schema do GraphQL
 const typeDefs = gql `
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+
+    }
+
+
     type Query {
         hello: String
+        notes: [Note!]!
+        note(id:ID!): Note!
     }
 `;
+
 // Funçoes resolver para os campos do schema criado anteriormente
 const resolvers = {
     Query: {
-        hello: () => 'Hello world'
+        hello: () => 'Hello world',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id)
+        }
     }
 }
 
@@ -25,7 +46,5 @@ const server = new ApolloServer({ typeDefs, resolvers })
 // Aplicando a Middleware do Apollo GraphQL e pondo o caminho para /api
 server.applyMiddleware({ app, path: '/api' })
 
-
-//app.get('/', (req, res) => { res.send('Hello World!!!') })
 
 app.listen({ port }, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`))
