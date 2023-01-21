8783834a5bdf5c7b9543dac0a3e2f5a1f0de41e0
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-01-21 20:37:10 -0300

    [DEV] Adding note model

---
+++ api/src/models/note.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Definindo o schema do banco de dados de notas
+const noteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+
+    }
+
+}, {
+    timestamp: true
+});
+
+// Definindo o modelo de Nota com o Schema
+const Note = mongoose.model('Note', noteSchema);
+
+// Exportando o modulo
+module.exports = Note;
