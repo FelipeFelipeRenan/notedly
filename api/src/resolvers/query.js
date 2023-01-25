2059cf38bf10e36151d28dd589d24d6ae8a1efeb
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-01-25 18:28:17 -0300

    [DEV] Adding new user queries

--- api/src/resolvers/query.js
+++ api/src/resolvers/query.js
@@ -1,8 +1,22 @@
 module.exports = {
     notes: async(parent, args, { models }) => {
-        return await models.Note.find()
+        return await models.Note.find();
     },
     note: async(parent, args, { models }) => {
-        return await models.Note.findById(args.id)
+        return await models.Note.findById(args.id);
+    },
+    user: async(parent, { username }, { models }) => {
+        // Encontrar um usuario dado um nome
+        return await models.User.findOne({ username });
+    },
+    users: async(parent, args, { models }) => {
+        // Encontrar todos os usuarios
+        return await models.User.find({});
+    },
+    me: async(parent, args, { models, user }) => {
+        // Encontrar um usuario dado o contexto do usuario atual
+        return await models.User.findById(user.id);
     }
+
+
 }
