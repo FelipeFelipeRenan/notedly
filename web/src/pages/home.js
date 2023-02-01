e26936bb684b60e3dfe7af250b219e533b7c4ac9
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-02-01 18:15:32 -0300

    [DEV] Adding GET_NOTES query to home

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,14 +1,41 @@
 import React from "react"
 import Button from "../components/Button"
 
+import { useQuery, gql } from "@apollo/client"
+
+// Query do GraphQl 
+const GET_NOTES = gql`
+    query NoteFeed($cursor: String){
+        noteFeed(cursor: $cursor){
+            cursor
+            hasNextPage
+            notes{
+                id
+                createdAt
+                content
+                favoriteCount
+                author{
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+
+`
+
 export default Home = () =>{
+
+    const {data, loading, error, fetchMore} = useQuery(GET_NOTES)
+    
+    loading?<p>Loading!</p>:<p>Error!</p>;
+    
     return(
         <div>
-        
-            <Button>Click me!</Button>
-            <p>This is the home page</p>
- 
-     
+            console.log(data);
+            The data loaded!
+
         </div>
     )
 }
