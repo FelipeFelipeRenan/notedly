600907f0bb86573b54e1a286ce4842701e837613
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-02-05 17:49:51 -0300

    [DEV] Adding sign up mutation

--- web/src/pages/signup.js
+++ web/src/pages/signup.js
@@ -1,6 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Button from "../components/Button";
+import { gql, useApolloClient, useMutation } from "@apollo/client";
+
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!){
+        signUp(email: $email, username: $username, password: $password)
+    }
+`
 
 const Wrapper = styled.div`
     border: 1px solid #ff5f4f0
@@ -21,19 +28,42 @@ const Form = styled.form`
     }
 
 `
-
-
 export default SignUp = props =>{
+
+    const [values, setValues] = useState()
+
+    const onChange = event => {
+        setValues({
+        ...values,
+        [event.target.name]: event.target.value
+        });
+        };
+
+
     useEffect(() =>{
         document.title = 'Sign up - Notedly'
 
 
     })
+    const [signUp,{loading, error}] = useMutation(SIGNUP_USER, {
+        onCompleted: data =>{
+            console.log(data.signUp);
+        }
+    })
 
     return(
         <Wrapper>
             <h2>Sign Up</h2>
-            <Form>
+            <Form
+                onSubmit={event =>{
+                    event.preventDefault()
+                    signUp({
+                        variables: {
+                            ...values
+                        }
+                    })
+                }}
+                >
                 <label htmlFor="username">Username:</label>
                 <input 
                     required
@@ -41,6 +71,7 @@ export default SignUp = props =>{
                     id="username"
                     name="username" 
                     placeholder="Username"
+                    onChange={onChange}
                 />
                 <label htmlFor="email">Email:</label>
                 <input 
@@ -49,6 +80,7 @@ export default SignUp = props =>{
                     id="email"
                     name="email" 
                     placeholder="Email"
+                    onChange={onChange}
                 />
                 <label htmlFor="password">Password</label>
                 <input 
@@ -56,6 +88,7 @@ export default SignUp = props =>{
                     id="password" 
                     name="password" 
                     placeholder="Password"
+                    onChange={onChange}
                 />
                 <Button type="submit">Submit</Button>
             </Form>
