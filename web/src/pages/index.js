c145a9bba5f550562b51025622ec0f5818f4430b
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-02-11 16:16:22 -0300

    [DEV] Creating PrivateRoute component

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route , Redirect} from "react-router-dom";
 import Home from "./home";
 import MyNotes from './mynotes'
 import Favorites from "./favorites";
@@ -7,14 +7,57 @@ import NotePage from "./note"
 import Layout from "../components/Layout";
 import SignUp from "./signup";
 import SignIn from "./signin";
+import { useQuery, gql } from "@apollo/client";
+
+const IS_LOGGED_IN = gql`
+    {
+        isLoggedIn @client
+    }
+
+`
+const PrivateRoute = ({component: Component, ...rest}) =>{
+    const {loading, error, data} = useQuery(IS_LOGGED_IN)
+
+    if(loading) return <p>Loading...</p>
+
+    if(error) return <p>Error!</p>
+
+    return(
+        <Route
+            {...rest}
+            render={props=>
+            data.isLoggedIn===true? (
+                <Component {...props}/>
+            ):(
+                <Redirect 
+                    to={{
+                        pathname:'/signin',
+                        state:{from: props.location}
+                    }}
+
+                
+                />
+            )
+        
+        
+        
+        }
+        
+        
+        
+        />
+    )
+
+
+}
 
 export default Pages = props =>{
     return(
         <Router>
             <Layout>
                 <Route exact path="/" component={Home}/>
-                <Route path="/mynotes" component={MyNotes}/>
-                <Route path="/favorites" component={Favorites}/>
+                <PrivateRoute path="/mynotes" component={MyNotes}/>
+                <PrivateRoute path="/favorites" component={Favorites}/>
                 <Route path="/note/:id" component={NotePage}/>
                 <Route path="/signup" component={SignUp} />
                 <Route path="/signin" component={SignIn} />
