26c2c6378ba4f3ac57081b995810f1e4fbae5c44
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-02-13 19:43:20 -0300

    [DEV] Adding new note mutation to note form

--- web/src/pages/new.js
+++ web/src/pages/new.js
@@ -2,10 +2,43 @@ import React, {useEffect} from "react";
 import { useMutation, gql } from "@apollo/client";
 import NoteForm from "../components/NoteForm";
 
+const NEW_NOTE = gql`
+    mutation newNote($content: String!){
+        newNote(content: $content){
+            id
+            content
+            createdAt
+            favoriteCount
+            favoritedBy{
+                id
+                username
+            }
+            author{
+                username
+                id
+            }
+        }
+    }
+
+`
+
 export default NewNote = props =>{
     useEffect(() =>{
         document.title = 'New Note - Notedly'
     })
 
-    return <NoteForm />
+    const [data, {loading, error}] = useMutation(NEW_NOTE,{
+        onCompleted: data =>{
+            props.history.push(`note/${note.NewNote.id}`)
+        }
+    })
+
+    return (
+    
+    <>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error saving the note</p>}
+        <NoteForm action={data}/>
+    </>
+    )
 }
